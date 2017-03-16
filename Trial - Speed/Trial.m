c3d5bf41515aff1a5d32ac386b4c3bac4a40398a
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-17 01:51:43 +0530

    Ran The Video Successfully

--- Trial - Speed/Trial.m
+++ Trial - Speed/Trial.m
@@ -1,5 +1,10 @@
 %=====================Reading Video Frame By Frame========================
 v = VideoReader('Camera Highway Surveillance.mp4');
-while hasframe(v)
-    video = readFrame(v);
+currAxes = axes;
+while hasFrame(v)
+    vidFrame = readFrame(v);
+    image(vidFrame, 'Parent', currAxes);
+    currAxes.Visible = 'off';
+    pause(1/v.FrameRate);
 end
+% =================== Successful =========================================
