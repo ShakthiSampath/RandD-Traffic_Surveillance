79423cbbab8ff4361b9f229ab036e4921331616e
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-17 01:59:21 +0530

    Update Trial Code

--- Trial - Speed/Trial.m
+++ Trial - Speed/Trial.m
@@ -1,6 +1,7 @@
 %=====================Reading Video Frame By Frame========================
 v = VideoReader('Camera Highway Surveillance.mp4');
-currAxes = axes;
+currAxes = axes;% axes made
+v.CurrentTime = 13;
 while hasFrame(v)
     vidFrame = readFrame(v);
     image(vidFrame, 'Parent', currAxes);
