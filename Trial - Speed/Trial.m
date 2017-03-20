2c4971dadc5c5a1f8711648b98df39e152c72b6b
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-20 22:59:14 +0530

    WIP Speed Trial

--- Trial - Speed/Trial.m
+++ Trial - Speed/Trial.m
@@ -1,6 +1,8 @@
 %===================== Declarations ======================================
 numFrames = 0;
+
 %===================== Reading Video Frame By Frame ======================
+
 v = VideoReader('Camera Highway Surveillance.mp4');
 currAxes = axes; % axes made
 v.CurrentTime = 13;
@@ -12,3 +14,25 @@ while hasFrame(v)
     numFrames = numFrames+1;
 end
 %disp(numFrames);
+
+% ==================== Function for Bounding Box ========================
+
+function B = B_box(frame)
+
+bg = frame.cdata;
+bg_bw = rgb2gray(bg);
+
+%----- set frame size variables -----
+
+fr_size = size(bg);
+wdth = fr_size(2);
+height = fr_size(1);
+fg = zeros(height, wdth);
+fg=uint8(fg);
+end
+
+
+
+function f = fact(n)
+    f = prod(1:n);
+end
