4de1afb336ecdc9fb549950c3a4e2107a7722eec
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-20 19:24:25 +0530

    Added Number of frames

--- Trial - Speed/Trial.m
+++ Trial - Speed/Trial.m
@@ -1,11 +1,16 @@
-%=====================Reading Video Frame By Frame========================
+%===================== Declarations ======================================
+numFrames = 0;
+
+%===================== Reading Video Frame By Frame ======================
 
 v = VideoReader('Camera Highway Surveillance.mp4');
 currAxes = axes;% axes made
 v.CurrentTime = 13;
 while hasFrame(v)
     vidFrame = readFrame(v);
+    numFrames = numFrames+1;
     image(vidFrame, 'Parent', currAxes);
     currAxes.Visible = 'off';
     pause(1/v.FrameRate);
 end
+% disp(numFrames);
