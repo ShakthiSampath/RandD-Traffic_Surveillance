5deb69bc0b8ddea27cfa1614edbb2930e0dd0454
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-23 00:54:53 +0530

    Updated Files

--- Trial - Speed/UsingOpticalFlow.m
+++ Trial - Speed/UsingOpticalFlow.m
@@ -1,9 +1,9 @@
 %Set up objects.
 
-videoReader = vision.VideoFileReader('Camera Highway Surveillance.mp4','ImageColorSpace','Intensity','VideoOutputDataType','uint8');
+videoReader = vision.VideoFileReader('Our2.mp4','ImageColorSpace','Intensity','VideoOutputDataType','uint8');
 converter = vision.ImageDataTypeConverter; 
 opticalFlow = vision.OpticalFlow('ReferenceFrameDelay', 1);
-opticalFlow.OutputValue = 'Horizontal and vertical components in complex form';
+%opticalFlow.OutputValue = 'Horizontal and vertical components in complex form';
 shapeInserter = vision.ShapeInserter('Shape','Lines','BorderColor','Custom', 'CustomBorderColor', 255);
 videoPlayer = vision.VideoPlayer('Name','Motion Vector');
 
@@ -12,8 +12,8 @@ videoPlayer = vision.VideoPlayer('Name','Motion Vector');
 while ~isDone(videoReader)
     frame = step(videoReader);
     im = step(converter, frame);
-    of = step(opticalFlow, im);
-    lines = videooptflowlines(of, 20);
+    VSQ = step(opticalFlow, im);
+    lines = videooptflowlines(VSQ, 20);
     if ~isempty(lines)
       out =  step(shapeInserter, im, lines); 
       step(videoPlayer, out);
